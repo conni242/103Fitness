cfaa91237f00207b013a337ee10e4857dedc59b5
conni242 <constantin.schlusche@googlemail.com>
2020-08-04 12:32:09 +0200

    add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase : {
+    apiKey: "AIzaSyDOAfknLCOCEnlCEwaoGPWPi327xkz-cQ8",
+    authDomain: "fitness-app-14a1d.firebaseapp.com",
+    databaseURL: "https://fitness-app-14a1d.firebaseio.com",
+    projectId: "fitness-app-14a1d",
+    storageBucket: "fitness-app-14a1d.appspot.com",
+    messagingSenderId: "164585649803",
+    appId: "1:164585649803:web:bd167080c672a6148a468e",
+    measurementId: "G-840VSRM7NJ"
+  }
 };
