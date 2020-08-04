42ec726bd31d4e74f05733fd0cb6528d7c4173ea
conni242 <constantin.schlusche@googlemail.com>
2020-08-04 11:36:27 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
+    AngularFireStorageModule, 
+    IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase : {
+    apiKey: "AIzaSyDOAfknLCOCEnlCEwaoGPWPi327xkz-cQ8",
+    authDomain: "fitness-app-14a1d.firebaseapp.com",
+    databaseURL: "https://fitness-app-14a1d.firebaseio.com",
+    projectId: "fitness-app-14a1d",
+    storageBucket: "fitness-app-14a1d.appspot.com",
+    messagingSenderId: "164585649803",
+    appId: "1:164585649803:web:bd167080c672a6148a468e",
+    measurementId: "G-840VSRM7NJ"
+  }
 };
 
 /*
